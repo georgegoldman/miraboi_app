c9b57be31f356ade3f7160a134068d7d88d81291
georgegoldman <georgegoldman2014@gmail.com>
2021-09-17 17:04:33 +0100

    1st commit to miraboi app

---
+++ src/App.vue
@@ -0,0 +1,164 @@
+<template>
+  <div id="app">
+
+    <div class="row h-100">
+      <div class="col-12 col-xl-6 col-sm-12 col-md-12 col-lg-6 col-xxl-6 pt-1 pb-2 p-md-0" id="profilebox">
+
+        <div class="pt-4 p-md-0">
+          <img src="https://scontent.fabb1-2.fna.fbcdn.net/v/t1.6435-9/237272282_370510311298178_5353209160180182899_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeFfOJ6gBGr-BbkFZyaQPHnRcVO0RwAe5VVxU7RHAB7lVViLELeWZoFPzFAzuYAS9DVpzRYtkHFOo0ynrbE7g3es&_nc_ohc=4utWBdiJEu8AX8FP10W&_nc_ht=scontent.fabb1-2.fna&oh=2f7412bed0e310054349a1ee1235d14d&oe=6167231B" id="profile_pix" class="rounded-circle p-sm-5 p-2 p-md-5" height="80%" alt="" srcset="">
+          <h4 style="font-family: 'Sacramento', cursive;"><b>Miraboi</b></h4>
+          <p><small>This Is The Official Website Of Miraboi</small></p>
+        </div>
+        
+      </div>
+
+      <div class="col-12 col-xl-12 col-sm-12 col-md-12 col-lg-6 col-xxl-6 pt-5 px-3" id="content" >
+        <p class="" id="content-text" style="font-family: 'Alike', serif;" >
+          For Business & Enquiries Kindly Reach Out To Any Of The Social Media Profile Below.
+        </p>
+
+        <img src="../public/img/right.svg" class="p-2 my-md-5 mt-3" id="arrow" alt="" srcset="">
+
+        <div class="d-flex justify-content-evenly">
+
+          <a href="https://twitter.com/miraboi"><i class="fab fa-3x fa-twitter"></i></a>
+
+          <a href="https://www.facebook.com/Miraboiofficial"><i class="fab fa-3x fa-facebook"></i></a>
+
+          <a href="https://www.instagram.com/miraboi"><i class="fab fa-3x fa-instagram"></i></a>
+
+        </div>
+
+        <h1 class="mt-md-4 my-5 " style="font-family: 'Mrs Saint Delafield', cursive;">Contact Me</h1>
+
+      </div>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+html, body {
+  height: 100%
+}
+body{
+  background-color: #000;
+  color: white;
+}
+#profilebox{
+  background-color: rgb(28, 28, 28);
+}
+
+a{
+  color: #fff !important;
+  text-decoration: none
+}
+
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {
+  #profile_pix{
+      width: 30% !important;
+      height: 30% !important;
+    }
+  #profilebox{
+    height: 31% !important;
+  }
+  #content{
+    height: 69%
+  }
+  #arrow{
+    height: 40%;
+    width: 30%
+  }
+  #content-text{
+    font-size: large;
+    text-align: left; 
+  }
+}
+
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {
+  #profile_pix{
+      width: 40%;
+      height: 40%
+    }
+
+    #profilebox{
+    height: 45% !important;
+  }
+  #content{
+    height: 6%
+  }
+    
+}
+
+/* Medium devices (landscape tablets, 768px and up) */
+@media only screen and (min-width: 768px) {
+
+
+    #profile_pix{
+      width: 27% !important;
+      height: 27% !important;
+    }
+  #profilebox{
+    height: 40% !important;
+  }
+  #content{
+    height: 60%;
+  }
+  #arrow{
+    height: 25%;
+    width: 25%
+  }
+  #content-text{
+    font-size: large
+  }
+
+}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {
+    #profile_pix{
+      width: 80%;
+    }
+}
+
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1024px) {
+  #profile_pix{
+        width: 80% !important;
+        height: 80% !important;
+      }
+    #profilebox{
+      height: 100% !important;
+    }
+    #content{
+      height: 100%;
+      font-size: xx-large
+    }
+    #arrow{
+      height: 25%;
+      width: 25%
+    }
+    #content-text{
+      font-size: x-large
+    }
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  height: 100%;
+}
+</style>
