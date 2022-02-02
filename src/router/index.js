7b7b29748154441f8b99c04ebb26847130ea9007
mrgeorgegoldman <georgegoldmanjohn.o@gmail.com>
2022-02-02 15:35:24 +0100

    updt

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Clients from '../views/Clients.vue'
 import Prservice from '../views/Prservice.vue'
+import OnDev from '../views/OnDev.vue'
 import VueMasonry from 'vue-masonry-css'
 
 Vue.use(VueMasonry);
@@ -19,17 +20,22 @@ const routes = [
     path: '/clients',
     name: 'Clients',
     component: Clients
-    // path: '/about',
-    // name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  //   // path: '/about',
+  //   // name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/pr_service',
     name: 'Pr_service',
     component: Prservice
+  }, 
+  {
+    path: '/on_dev',
+    name: 'OnDev',
+    component: OnDev
   }
 ]
 
---
+++ src/views/OnDev.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="position-relative h-100">
+        <div class="position-absolute top-50 start-50 translate-middle">
+            <h1>
+                <img :src="ondevImage" alt="" srcset="">
+                <p>This Page is under Revamping</p>
+            </h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'OnDev',
+    data () {
+        return {
+            ondevImage: '../img/brick-wall.png'
+        }
+    }
+}
+</script>
+
+<style >
+#onde{
+      text-align: center;
+}
+</style>
